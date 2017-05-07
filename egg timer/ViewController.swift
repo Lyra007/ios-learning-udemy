0bc472e66bb3bd7ed78f18ff2ed07b076bc40d5d
runningstrawberry <elfishing07@gmail.com>
2017-05-07 21:10:42 +0800

    my very first egg timer

--- egg timer/ViewController.swift
+++ egg timer/ViewController.swift
@@ -10,10 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer=Timer()
+    
+    @IBOutlet var numberLabel: UILabel!
+    
+    
+    
+    @IBAction func onPause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func onPlay(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.onPlaying), userInfo: nil, repeats: true)
+    }
+    
+    func onPlaying (){
+        var num = Int(numberLabel.text!)!
+        num = num - 1
+        numberLabel.text = String(num)
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        var num = Int(numberLabel.text!)!
+        if(num>=10){
+            num = num - 10
+            numberLabel.text = String(num)
+        }
+
+    }
+    
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        timer.invalidate()
+        numberLabel.text = String(210)
+    }
+    
+    
+    @IBAction func plusTen(_ sender: Any) {
+        var num = Int(numberLabel.text!)!
+            num = num + 10
+            numberLabel.text = String(num)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
